f161feb5c6c53c6dfc1dbdfc494871ee77bc3144
Sudhikshapoojary <sudhiksha.23cs166@sode-edu.in>
2025-11-30 19:15:53 +0530

    SA

---
+++ models.py
@@ -0,0 +1,35 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Patient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    age = db.Column(db.Integer)
+    gender = db.Column(db.String(10))
+    contact = db.Column(db.String(50))
+
+class Doctor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    specialization = db.Column(db.String(120))
+    contact = db.Column(db.String(50))
+
+class Medicine(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    stock = db.Column(db.Integer, default=0)
+
+class MedicalRecord(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    patient_id = db.Column(db.Integer, db.ForeignKey('patient.id'), nullable=False)
+    doctor_id = db.Column(db.Integer, db.ForeignKey('doctor.id'))
+    diagnosis = db.Column(db.String(500))
+    prescription = db.Column(db.String(500))
+
+class Bill(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    patient_id = db.Column(db.Integer, db.ForeignKey('patient.id'), nullable=False)
+    total = db.Column(db.Float, nullable=False)
+    details = db.Column(db.String(1000))
